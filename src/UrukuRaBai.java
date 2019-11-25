d046cd7d5bbc39c6b23338221208e0e31228ef85
srikaryagavandla <yagavandla.s@husky.neu.edu>
2019-11-25 16:31:32 -0500

    cdoe changes

--- src/UrukuRaBai.java
+++ src/UrukuRaBai.java
@@ -1,19 +1,19 @@
-//import java.io.FileNotFoundException;
-//import java.io.FileReader;
-//
-//import controller.Controller;
-//import controller.ImageProcessorController;
-//import model.ModelFactory;
-//import model.ModelFactoryImpl;
-//
-//public class UrukuRaBai {
-//
-//  public static void main(String[] args) throws FileNotFoundException {
-//    String filePath = args[0];
-//    Readable readable = new FileReader(filePath);
-//    ModelFactory factory=new ModelFactoryImpl();
-//    Controller controller = new ImageProcessorController(readable,factory);
-//    controller.process();
-//  }
-//}
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import controller.Controller;
+import controller.ImageProcessorController;
+import model.ModelFactory;
+import model.ModelFactoryImpl;
+
+public class UrukuRaBai {
+
+  public static void main(String[] args) throws FileNotFoundException {
+    String filePath = args[0];
+    Readable readable = new FileReader(filePath);
+    ModelFactory factory = new ModelFactoryImpl();
+    Controller controller = new ImageProcessorController(readable, factory);
+    controller.process();
+  }
+}
 
--- src/controller/ImageProcessorController.java
+++ src/controller/ImageProcessorController.java
@@ -1,178 +1,133 @@
-//package controller;
-//
-//import java.awt.image.BufferedImage;
-//import java.io.File;
-//import java.io.IOException;
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Scanner;
-//
-//import javax.imageio.ImageIO;
-//
-//import control.ImageGenerationCommand;
-//import control.ImageManipulationCommand;
-//import control.ImageProcessingCommand;
-//import control.commands.Generation.CheckeredBoard;
-//import control.commands.Generation.FranceFlag;
-//import control.commands.Generation.GreeceFlag;
-//import control.commands.Generation.HorizontalRainbow;
-//import control.commands.Generation.Switzerland;
-//import control.commands.Generation.VerticalRainbow;
-//import control.commands.Manipulation.Blur;
-//import control.commands.Manipulation.Dither;
-//import control.commands.Manipulation.GreyScale;
-//import control.commands.Manipulation.Mosaic;
-//import control.commands.Manipulation.Sepia;
-//import control.commands.Manipulation.Sharpen;
-//import model.ImageGenerator;
-//import model.ImageManipulation;
-//import model.ImageModel;
-//import model.ImageProcessor;
-//import model.ModelFactory;
-//
-//public class ImageProcessorController implements Controller {
-//
-//  private Readable readable;
-//  private final ModelFactory factory;
-//
-//  public ImageProcessorController(Readable readable,ModelFactory factory) {
-//    this.readable = readable;
-//    this.factory=factory;
-//  }
-//
-//  @Override
-//  public void process() {
-//    List<String> commands = readData();
-//    ImageModel model = null;
-//    ImageManipulation manipulation=model;
-//    ImageGenerator generator=model;
-//    ImageProcessingCommand cmd = null;
-//    ImageManipulationCommand manipulationCommand=null;
-//    ImageGenerationCommand generationCommand=null;
-//    for (int i = 0; i < commands.size(); i++) {
-//      String line = commands.get(i);
-//      switch (line) {
-//        case "q":
-//          return;
-//        case "load":
-//          String filePath = commands.get(i + 1);
-//          i++;
-//          BufferedImage img = readImage(filePath);
-//          manipulation = factory.getManipulation(img);
-////          manipulationCommand.process(manipulation);
-////          generator=factory.getManipulation(img);
-//          break;
-//        case "mosaic":
-//          int seedCount = Integer.parseInt(commands.get(i + 1));
-//          i++;
-//          cmd = new Mosaic(seedCount);
-//          break;
-//        case "dither":
-//          cmd = new Dither();
-//          break;
-//        case "sepia":
-//          cmd = new Sepia();
-//          break;
-//        case "greyscale":
-//          cmd = new GreyScale();
-//          break;
-//        case "blur":
-//          cmd = new Blur();
-//          break;
-//        case "France":
-//          int width = Integer.parseInt(commands.get(i + 1));
-//          i++;
-////          model = new ImageProcessor(width, (2 * width) / 3);
-//          model =factory.getGeneration(width, (2 * width) / 3);
-//          cmd = new FranceFlag(width);
-//          break;
-//        case "Switzerland":
-//          width = Integer.parseInt(commands.get(i + 1));
-//          i++;
-//          int height = width;
-//          model = new ImageProcessor(width, height);
-//          cmd = new Switzerland(width);
-//          break;
-//        case "sharpen":
-//          cmd = new Sharpen();
-//          break;
-//        case "Horizontal Rainbow":
-//          int thickness = Integer.parseInt(commands.get(i + 1));
-//          i++;
-//          int length = Integer.parseInt(commands.get(i + 1));
-//          i++;
-//          model = new ImageProcessor(length, thickness * 7);
-//          cmd = new HorizontalRainbow(length, thickness);
-//          break;
-//        case "Vertical Rainbow":
-//          int bandwidth = Integer.parseInt(commands.get(i + 1));
-//          i++;
-//          height = Integer.parseInt(commands.get(i + 1));
-//          i++;
-//          model = new ImageProcessor(bandwidth * 7, height);
-//          cmd = new VerticalRainbow(height, bandwidth);
-//          break;
-//        case "CheckeredBoard":
-//          int size = Integer.parseInt(commands.get(i + 1));
-//          i++;
-//          model = new ImageProcessor(8 * size, 8 * size);
-//          cmd = new CheckeredBoard(size);
-//          break;
-//        case "Greece":
-//          height = Integer.parseInt(commands.get(i + 1));
-//          i++;
-//          height = height / 18 * 18;
-//          width = 3 * height / 2;
-//          model = new ImageProcessor(width, height);
-//          cmd = new GreeceFlag(width, height);
-//          break;
-//        case "save":
-//          filePath = commands.get(i + 1);
-//          i++;
-//          BufferedImage result = model.getImage();
-//          writeImage(result, filePath);
-//          break;
-//        default:
-//          System.out.println("Invalid operation");
-//          break;
-//      }
-//      if (cmd != null) {
-//        cmd.process(model);
-//        System.out.println(".....Done.");
-//        cmd = null;
-//      }
-//    }
-//  }
-//
-//  private List<String> readData() {
-//    Scanner scanner = new Scanner(readable);
-//    List<String> commands = new ArrayList<>();
-//    while (scanner.hasNextLine()) {
-//      commands.add(scanner.nextLine());
-//    }
-//
-//    return commands;
-//  }
-//
-//  private BufferedImage readImage(String filepath) {
-//    BufferedImage image = null;
-//    File f;
-//    try {
-//      f = new File(filepath);
-//      image = ImageIO.read(f);
-//    } catch (IOException e) {
-//      System.out.println(e.getMessage());
-//    }
-//    return image;
-//  }
-//
-//  private void writeImage(BufferedImage img, String filepath) {
-//    try {
-//      File output = new File(filepath);
-//      ImageIO.write(img, "png", output);
-//    } catch (IOException e) {
-//      System.out.println(e.getMessage());
-//    }
-//    System.out.println("Image written to :" + filepath);
-//  }
-//}
+package controller;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.function.Function;
+
+import javax.imageio.ImageIO;
+
+import control.ImageGenerationCommand;
+import control.ImageManipulationCommand;
+import control.commands.Generation.CheckeredBoard;
+import control.commands.Generation.FranceFlag;
+import control.commands.Generation.GreeceFlag;
+import control.commands.Generation.HorizontalRainbow;
+import control.commands.Generation.Switzerland;
+import control.commands.Generation.VerticalRainbow;
+import control.commands.Manipulation.Blur;
+import control.commands.Manipulation.Dither;
+import control.commands.Manipulation.GreyScale;
+import control.commands.Manipulation.Mosaic;
+import control.commands.Manipulation.Sepia;
+import control.commands.Manipulation.Sharpen;
+import model.ImageGenerator;
+import model.ImageManipulation;
+import model.ImageModel;
+import model.ModelFactory;
+
+public class ImageProcessorController implements Controller {
+
+  private Readable readable;
+  private final ModelFactory factory;
+
+  public ImageProcessorController(Readable readable, ModelFactory factory) {
+    this.readable = readable;
+    this.factory = factory;
+  }
+
+  @Override
+  public void process() {
+    ImageModel model = null;
+    Map<String, Function<Scanner, ImageGenerationCommand>> generationKnownCommands =
+            setupGenerationKnownCommands();
+    Map<String, Function<Scanner, ImageManipulationCommand>> manipulationKnownCommands =
+            setupManipulationKnownCommands();
+
+    Scanner scanner = new Scanner(readable);
+    //List<String> commands = getCommands();
+    while (scanner.hasNextLine()) {
+      String command = scanner.nextLine();
+      if (command.equals("load")) {
+        BufferedImage image = readImage(scanner.nextLine());
+        model = factory.getImageModel(image);
+      } else if (command.equals("save")) {
+        writeImage(model.getImage(), scanner.nextLine());
+      } else {
+        if (generationKnownCommands.containsKey(command) ||
+                manipulationKnownCommands.containsKey(command)) {
+          if (generationKnownCommands.containsKey(command)) {
+            model = factory.getImageModel(scanner.nextInt(), scanner.nextInt());
+            Function<Scanner, ImageGenerationCommand> function = generationKnownCommands.get(command);
+            function.apply(scanner).process(model);
+          } else {
+            manipulationKnownCommands.get(command).apply(scanner).process(model);
+          }
+        }
+      }
+    }
+  }
+
+  private List<String> getCommands() {
+    Scanner scanner = new Scanner(readable);
+    List<String> commands = new ArrayList<>();
+    while (scanner.hasNextLine()) {
+      commands.add(scanner.nextLine());
+    }
+
+    return commands;
+  }
+  private BufferedImage readImage(String filepath) {
+    try {
+      return ImageIO.read(new File(filepath));
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    }
+    return null;
+  }
+
+  private void writeImage(BufferedImage img, String filepath) {
+    try {
+      File output = new File(filepath);
+      ImageIO.write(img, "png", output);
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    }
+    System.out.println("Image written to :" + filepath);
+  }
+
+  private Map<String, Function<Scanner, ImageGenerationCommand>> setupGenerationKnownCommands() {
+    Map<String, Function<Scanner,
+            ImageGenerationCommand>> generationKnownCommands = new HashMap<>();
+    generationKnownCommands.put("Switzerland", scanner -> new Switzerland(scanner.nextInt()));
+    generationKnownCommands.put("France", scanner -> new FranceFlag(scanner.nextInt()));
+    generationKnownCommands.put("Horizontal Rainbow", scanner -> new HorizontalRainbow(
+            scanner.nextInt(), scanner.nextInt()));
+    generationKnownCommands.put("Vertical Rainbow", scanner -> new
+            VerticalRainbow(scanner.nextInt(), scanner.nextInt()));
+    generationKnownCommands.put("CheckeredBoard", scanner ->
+            new CheckeredBoard(scanner.nextInt()));
+    generationKnownCommands.put("Greece", scanner ->
+            new GreeceFlag(scanner.nextInt(), scanner.nextInt()));
+    return generationKnownCommands;
+  }
+
+  private Map<String, Function<Scanner, ImageManipulationCommand>> setupManipulationKnownCommands() {
+    Map<String, Function<Scanner, ImageManipulationCommand>>
+            manipulationKnownCommands = new HashMap<>();
+    manipulationKnownCommands.put("mosaic", scanner -> new Mosaic(scanner.nextInt()));
+    manipulationKnownCommands.put("dither", scanner -> new Dither());
+    manipulationKnownCommands.put("sepia", scanner -> new Sepia());
+    manipulationKnownCommands.put("greyscale", scanner -> new GreyScale());
+    manipulationKnownCommands.put("blur", scanner -> new Blur());
+    manipulationKnownCommands.put("sharpen", scanner -> new Sharpen());
+
+    return manipulationKnownCommands;
+  }
+}
+
